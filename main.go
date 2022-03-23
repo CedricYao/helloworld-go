433a3d95e6e749b4ef8a63ef98ca84d543d2d632
Cedric Yao <ycedric@ycedric-a02.vmware.com>
2022-03-23 15:57:13 -0600

    turning hello world into web service

--- main.go
+++ main.go
@@ -3,8 +3,19 @@ package main
 import (
 	"fmt"
 	"helloworld-go/gowebservice"
+	"net/http"
 )
 
 func main() {
-	fmt.Println(gowebservice.HelloWorld())
+	fmt.Println("web server is running on :8080")
+	HelloWorldWebServer()
+}
+
+func HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, gowebservice.HelloWorld())
+}
+
+func HelloWorldWebServer() {
+	http.HandleFunc("/", HelloWorldHandler)
+	http.ListenAndServe(":8080", nil)
 }
